56d9dae41223c1cc0fda12f4b898ea1ca15ee979
adityagoel28 <adityagoel2002@gmail.com>
2023-12-28 21:25:17 +0530

    Validated Last Name

--- src/main/java/UserRegistrationRegex/Validator.java
+++ src/main/java/UserRegistrationRegex/Validator.java
@@ -6,4 +6,8 @@ public class Validator {
     public static boolean validateFirstName(String firstName) {
         return Pattern.matches(Regex.FIRST_NAME_PATTERN, firstName);
     }
+
+    public static boolean validateLastName(String lastName) {
+        return Pattern.matches(Regex.LAST_NAME_PATTERN, lastName);
+    }
 }
