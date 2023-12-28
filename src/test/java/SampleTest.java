b792b080f832ab709d8fb38769d04fec4ebe0472
adityagoel28 <adityagoel2002@gmail.com>
2023-12-28 22:00:33 +0530

    Write JUnit Test to validate the User Entry for First Name, Last Name, Email, Mobile, and Password

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -43,4 +43,34 @@ public class SampleTest {
             assertFalse(UserRegistrationRegex.Validator.validateEmail(email));
         }
     }
+
+    @Test
+    public void testFirstNameValid() {
+        assertTrue(UserRegistrationRegex.Validator.validateFirstName("Aditya"));
+        assertFalse(UserRegistrationRegex.Validator.validateFirstName("ad"));
+    }
+
+    @Test
+    public void testLastNameValid() {
+        assertTrue(UserRegistrationRegex.Validator.validateLastName("Goel"));
+        assertFalse(UserRegistrationRegex.Validator.validateLastName("goel"));
+    }
+
+    @Test
+    public void testEmailValid() {
+        assertTrue(UserRegistrationRegex.Validator.validateEmail("adityagoel2002@gmail.com"));
+        assertFalse(UserRegistrationRegex.Validator.validateEmail("adityagoel2002@gmail"));
+    }
+
+    @Test
+    public void testMobileValid() {
+        assertTrue(UserRegistrationRegex.Validator.validateMobile("91 1234567890"));
+        assertFalse(UserRegistrationRegex.Validator.validateMobile("911234567890"));
+    }
+
+    @Test
+    public void testPasswordValid() {
+        assertTrue(UserRegistrationRegex.Validator.validatePassword("Aditya@123"));
+        assertFalse(UserRegistrationRegex.Validator.validatePassword("aditya123"));
+    }
 }
